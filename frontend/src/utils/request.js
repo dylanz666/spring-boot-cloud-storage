34845a011488cbdac9abfe04abe956d1b539cdaa
dylanz666 <yan914017hui>
2020-11-28 22:46:43 +0800

    适配手机

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -4,7 +4,7 @@ import { Message, MessageBox } from 'element-ui'
 // 创建axios实例
 const service = axios.create({
   baseURL: process.env.BASE_API, // api的base_url
-  timeout: 15000 // 请求超时时间
+  timeout: 3600000 // 请求超时时间
 })
 
 // request拦截器
